f5c41a1852ff933ab1b2eeccc5b87e50246dce44
Akatronix <153951875+Akatronix@users.noreply.github.com>
2025-02-04 12:49:59 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,71 +1,3 @@
-// const socket = io.connect(window.location.origin);
-
-// let myStream;
-// let peer;
-// const myVideo = document.getElementById("myVideo");
-// const remoteVideo = document.getElementById("remoteVideo");
-
-// // Get user media (video & audio)
-// navigator.mediaDevices
-//   .getUserMedia({ video: true, audio: true })
-//   .then((stream) => {
-//     myStream = stream;
-//     myVideo.srcObject = stream;
-//     myVideo.muted = true; // Prevent self-audio loop
-
-//     socket.emit("joinRoom"); // Notify server that user joined
-
-//     socket.on("userConnected", (otherUserId) => {
-//       console.log("New user connected:", otherUserId);
-
-//       // Create a peer connection (initiator = true means this user is calling)
-//       peer = new SimplePeer({
-//         initiator: true,
-//         trickle: false,
-//         stream: myStream,
-//       });
-
-//       peer.on("signal", (data) => {
-//         socket.emit("sendOffer", { target: otherUserId, signalData: data });
-//       });
-
-//       peer.on("stream", (userStream) => {
-//         remoteVideo.srcObject = userStream;
-//       });
-
-//       peer.on("error", (err) => console.error("Peer Error:", err));
-//     });
-
-//     socket.on("offerReceived", ({ from, signal }) => {
-//       console.log("Offer received from:", from);
-
-//       // Create a peer connection (initiator = false means this user is answering)
-//       peer = new SimplePeer({
-//         initiator: false,
-//         trickle: false,
-//         stream: myStream,
-//       });
-
-//       peer.on("signal", (data) => {
-//         socket.emit("sendAnswer", { target: from, signalData: data });
-//       });
-
-//       peer.on("stream", (userStream) => {
-//         remoteVideo.srcObject = userStream;
-//       });
-
-//       peer.on("error", (err) => console.error("Peer Error:", err));
-
-//       // Accept incoming WebRTC signal
-//       peer.signal(signal);
-//     });
-
-//     socket.on("answerReceived", (signal) => {
-//       console.log("Answer received, completing connection");
-//       peer.signal(signal);
-//     });
-
-
 const socket = io.connect(window.location.origin);
 
 let myStream;
@@ -193,11 +125,8 @@ function saveRecording() {
 
 // Get Video Streams and Start Recording
 document.getElementById("startRecording").addEventListener("click", () => {
-  const localVideo = document.getElementById("myVideo");
-  const remoteVideo = document.getElementById("remoteVideo");
-
-  if (localVideo.srcObject && remoteVideo.srcObject) {
-    startRecording(localVideo.srcObject, remoteVideo.srcObject);
+  if (myStream && remoteVideo.srcObject) {
+    startRecording(myStream, remoteVideo.srcObject); // Use streams directly
   } else {
     alert("No video streams available!");
   }
